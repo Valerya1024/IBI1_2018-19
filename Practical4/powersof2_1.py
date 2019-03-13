a094b3fab9df413076069fa9f47efe98462d6a6d
Valerya <13588262185@163.com>
2019-03-13 22:37:56 +0800

    simplified po2_1

--- Practical4/powersof2_1.py
+++ Practical4/powersof2_1.py
@@ -6,28 +6,21 @@ Created on Wed Mar 13 14:13:15 2019
 """
 
 from random import randint
-#select random integer n
-n = randint(1,2**13)
-#store the origin integer in N
-N = str(n)
+#select random integer n, store the initial value in m
+n = m = randint(1,2**13)
 #x as exponent
-x = 13
 #creat a list to store x
 list = []
 #find the biggest power of 2 that is smaller than n
-for i in range(1,15):
+for x in range(12,-1,-1):
     #if n is smaller than 2**x, skip x
-    if n < 2**x:
-        x = x - 1
     #if n is larger, store x
-    elif n > 2**x:
+    if  n > 2**x:
+        list.append("2**"+str(x))
+        n = n - 2**x
+    #if n = 2**x, print x, end process
+    elif n == 2**x:
         n = n - 2**x
         list.append("2**"+str(x))
-        #next x
-        x = x - 1
-    #if n = 2**x, store x, end process
-    else:
-        list.append("2**"+str(x))
-        break
 #join the list to a string
-print(N, "is", ' + '.join(list))
+print(str(m), "is", ' + '.join(list))
