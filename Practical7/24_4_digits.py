3ee8368c5ed49ac1083677c8c106a27b9684f7db
Valerya <13588262185@163.com>
2019-04-05 13:21:06 +0800

    added function

--- Practical7/24_4_digits.py
+++ Practical7/24_4_digits.py
@@ -6,46 +6,51 @@ Created on Wed Apr  3 09:02:08 2019
 """
 
 import itertools
-import re
+L = input("Please input 4 numbers to compute 24:(use ',' to divide them)\n")
 def compute(x,y,Op):
-    if Op=='+':return x+y
-    elif Op=='*':return x*y
-    elif Op=='-':return x-y
-    else:return x/y if y != 0 else None
-L = re.split(r',',input("Please input 4 numbers to compute 24:(use ',' to divide them)\n"))
-L = list(map(int,L))
-if len(L) != 4 :
-    print("Please try in file 24_n_digits.py" )
-elif max(L)>23 or min(L)<1:
-    print("The input number must be integers from 1 to 23")
+        if Op == '+': return x+y
+        elif Op == '*': return x*y
+        elif Op == '-': return x-y
+        else: return x/y if y != 0 else None
+if "." in L or "/" in L:
+    print("The input number must be integers")
 else:
-    sequence = list(itertools.permutations(L,4))[::]
-    operations = ['+','-','*','/']
-    op = list(itertools.combinations_with_replacement(operations,3))[::]
-    operator = []
-    for i in range(len(op)-1):
-        operator += list(itertools.permutations(op[i],3))[::]
-    operator = sorted(set(operator))
-    x = 0
-    y = 0
-    for i in range(len(sequence)-1):
-        S = sequence[i]
-        for i in range(len(operator)-1):
-            O = operator[i]
-            a = compute(S[0],S[1],O[0])
-            b = compute(a,S[2],O[1])
-            c = compute(S[2],S[3],O[2])
-            if compute(b,S[3],O[2]) == 24:
-                x += 1
-                break
-            elif compute(a,c,O[1]) == 24:
-                x += 1
-                break
-            else:
-                y += 1
-    if x != 0:
-        print("Yes")
+    L = L.split(",")
+    num = list(map(int,L))
+    if len(num) != 4:
+        print("Please try in file 24_n_digits.py" )
+    elif max(num)>23 or min(num)<1:
+        print("The input number must be integers from 1 to 23")
     else:
-        print("No")
-    print("Recursion times:"+str(y+1))
+        sequence = list(itertools.permutations(num,4))[::]
+        operations = ['+','-','*','/']
+        op = list(itertools.combinations_with_replacement(operations,3))[::]
+        operator = []
+        for i in range(len(op)-1):
+            operator += list(itertools.permutations(op[i],3))[::]
+        operator = sorted(set(operator))
+        x = y = 0
+        for i in range(len(sequence)-1):
+            if x == 0:
+                S = sequence[i]
+                for i in range(len(operator)-1):
+                    O = operator[i]
+                    a = compute(S[0],S[1],O[0])
+                    b = compute(a,S[2],O[1])
+                    c = compute(S[2],S[3],O[2])
+                    if compute(compute(compute(S[0],S[1],O[0]),S[2],O[1]),S[3],O[2]) == 24:
+                        x += 1
+                        print("(("+str(S[0])+O[0]+str(S[1])+")"+O[1]+str(S[2])+")"+O[2]+str(S[3]))
+                        break
+                    elif compute(compute(S[0],S[1],O[0]),compute(S[2],S[3],O[2]),O[1]) == 24:
+                        print("("+str(S[0])+O[0]+str(S[1])+")"+O[1]+"("+str(S[2])+O[2]+str(S[3])+")")
+                        x += 1
+                        break
+                    else:
+                        y += 1
+        if x != 0:
+            print("Yes")
+        else:
+            print("No")
+        print("Recursion times:"+str(y+1))
         
--- Practical7/24_n_digits.py
+++ Practical7/24_n_digits.py
@@ -5,59 +5,60 @@ Created on Wed Apr  3 09:02:08 2019
 @author: Valerya, inspired by yangjiaolei
 """
 
-import re
 from fractions import Fraction
-L = re.split(r',',input("Please input numbers to compute 24:(use ',' to divide them)\n"))
-num = list(map(int,L))
-if max(num)>23 or min(num)<1:
-    print("The input number must be integers from 1 to 23")
+L = input("Please input numbers to compute 24:(use ',' to divide them)\n")
+if "." in L or "/" in L:
+    print("The input number must be integers")
 else:
-    times = 0
-    def cal(n):
-        global times
-        times += 1
-        if n == 1:
-            if float(num[0]) == 24:
-                return 1
+    L = L.split(",")
+    num = list(map(int,L))
+    if max(num)>23 or min(num)<1:
+        print("The input number must be integers from 1 to 23")
+    else:
+        times = 0
+        def cal(n):
+            global times
+            times += 1
+            if n == 1:
+                if float(num[0]) == 24:
+                    return 1
+                else:
+                    return 0
             else:
-                return 0
-        else:
-            for i in range(n):
-                for j in range(i+1,n):
-                    a = num[i]
-                    b = num[j]
-                    num[j] = num[n-1]
+                for i in range(n):
+                    for j in range(i+1,n):
+                        a = num[i]
+                        b = num[j]
+                        num[j] = num[n-1]
                     
-                    num[i] = a+b
-                    if cal(n-1):
-                        return 1
-                    num[i] = a-b
-                    if cal(n-1):
-                        return 1
-                    
-                    num[i] = b-a
-                    if cal(n-1):
-                        return 1
-                    
-                    num[i] = a*b
-                    if cal(n-1):
-                        return 1
-                    if a != 0:
-                        num[i] = Fraction(b,a)
+                        num[i] = a+b
                         if cal(n-1):
                             return 1
-                    if b != 0:
-                        num[i] = Fraction(a,b)
+                        num[i] = a-b
                         if cal(n-1):
                             return 1
-                    num[i] = a
-                    num[j] = b
+                    
+                        num[i] = b-a
+                        if cal(n-1):
+                            return 1
+                    
+                        num[i] = a*b
+                        if cal(n-1):
+                            return 1
+                        if a != 0:
+                            num[i] = Fraction(b,a)
+                            if cal(n-1):
+                                return 1
+                        if b != 0:
+                            num[i] = Fraction(a,b)
+                            if cal(n-1):
+                                return 1
+                        num[i] = a
+                        num[j] = b
             return 0
     
-    if cal(len(num)):
-        print("Yes")
-    else:
-        print("No")
-    print("Recursion times:"+str(times))
-        
-        
+        if cal(len(num)):
+            print("Yes")
+        else:
+            print("No")
+        print("Recursion times:"+str(times))
