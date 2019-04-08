ef61473207dfaebf7e84521cbe7dfef65cb9175d
Valerya <13588262185@163.com>
2019-04-08 12:44:50 +0800

    corrected bug

--- Practical7/24_4_digits.py
+++ Practical7/24_4_digits.py
@@ -2,16 +2,20 @@
 """
 Created on Wed Apr  3 09:02:08 2019
 
-@author: surface
+@author: Valerya
 """
 
 import itertools
+from fractions import Fraction
 L = input("Please input 4 numbers to compute 24:(use ',' to divide them)\n")
 def compute(x,y,Op):
+    if x == None or y == None:
+        return None
+    else:
         if Op == '+': return x+y
         elif Op == '*': return x*y
         elif Op == '-': return x-y
-        else: return x/y if y != 0 else None
+        else: return Fraction(x,y) if y != 0 else None
 if "." in L or "/" in L:
     print("The input number must be integers")
 else:
@@ -19,7 +23,7 @@ else:
     num = list(map(int,L))
     if len(num) != 4:
         print("Please try in file 24_n_digits.py" )
-    elif max(num)>23 or min(num)<1:
+    elif max(num)>13 or min(num)<1:
         print("The input number must be integers from 1 to 23")
     else:
         sequence = list(itertools.permutations(num,4))[::]
@@ -35,19 +39,29 @@ else:
                 S = sequence[i]
                 for i in range(len(operator)-1):
                     O = operator[i]
-                    if compute(compute(compute(S[0],S[1],O[0]),S[2],O[1]),S[3],O[2]) == 24:
+                    if compute(compute(compute(S[0],S[1],O[0]),S[2],O[1]),S[3],O[2]) == 24.0:
                         x += 1
                         print("(("+str(S[0])+O[0]+str(S[1])+")"+O[1]+str(S[2])+")"+O[2]+str(S[3]))
                         break
-                    elif compute(compute(S[0],S[1],O[0]),compute(S[2],S[3],O[2]),O[1]) == 24:
+                    elif compute(compute(S[0],S[1],O[0]),compute(S[2],S[3],O[2]),O[1]) == 24.0:
                         print("("+str(S[0])+O[0]+str(S[1])+")"+O[1]+"("+str(S[2])+O[2]+str(S[3])+")")
-                        x += 1
+                        x += 2
                         break
+                    elif compute(compute(S[0],compute(S[1],S[2],O[1]),O[0]),S[3],O[2]) == 24.0:
+                        print("("+str(S[0])+O[0]+"("+str(S[1])+O[1]+str(S[2])+"))"+O[2]+str(S[3]))
+                        x += 3
+                        break
+                    elif compute(S[0],compute(S[1],compute(S[2],S[3],O[2]),O[1]),O[0]) == 24.0:
+                        print(str(S[0])+O[0]+"("+str(S[1])+O[1]+"("+str(S[2])+O[2]+str(S[3])+"))")
+                        x += 4
+                    elif compute(S[0],compute(compute(S[1],S[2],O[1]),S[3],O[2]),O[0]) == 24.0:
+                        print(str(S[0])+O[0]+"(("+str(S[1])+O[1]+str(S[2])+")"+O[2]+str(S[3])+")")
+                        x += 5
                     else:
-                        y += 1
+                        y += 5
         if x != 0:
             print("Yes")
         else:
             print("No")
-        print("Recursion times:"+str(y+1))
+        print("Recursion times:"+str(y+5))
         
