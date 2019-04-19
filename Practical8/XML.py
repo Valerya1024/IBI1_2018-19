d3f43455235b595e16248e4740083a63f9f8b385
Valerya <13588262185@163.com>
2019-04-19 13:41:16 +0800

    Submit Pr8

---
+++ Practical8/XML.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 10 09:05:41 2019
+
+@author: surface
+"""
+import re
+import xml.dom
+import pandas as pd
+
+DOM = xml.dom.minidom.parse('go_obo.xml')
+obo = DOM.documentElement
+genes = obo.getElementsByTagName('term')
+
+def findchildren(id,children):
+    for term in genes:
+            parents = term.getElementsByTagName('is_a')
+            for parent in parents:
+                if parent.childNodes[0].data == id:
+                    childid = term.getElementsByTagName('id')
+                    children.append(childid)
+                    findchildren(childid,children)
+    return len(children)
+                
+autophagosome = pd.DataFrame(columns = ['id','name','definition','childnodes'])
+
+for term in genes:
+    defstr = term.getElementsByTagName('defstr')[0].childNodes[0].data
+    if re.search(r'autophagosome', defstr):
+        id = term.getElementsByTagName('id')[0].childNodes[0].data
+        name = term.getElementsByTagName('name')[0].childNodes[0].data
+        children = []
+        autophagosome = autophagosome.append(pd.DataFrame({'id':[id],'name':[name],'definition':[defstr],'childnodes':[findchildren(id,children)]}))
+        
+autophagosome.to_excel('C:/Users/surface/.spyder-py3/python script/Practical 8/autophagosome.xlsx', index=False)
